d9e49966ef192dae49139f0226f5617b8ca65cde
Torben Tietze <torben.tietze@googlemail.com>
2019-12-31 14:59:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,216 @@
+import sys
+from telegram.ext import Updater
+import logging
+import shelve
+from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
+import requests
+import json
+from datetime import datetime, timedelta
+import datetime as d
+import copy
+import os
+import math
+from enum import Enum
+
+credentials = {}
+db_file = 'user.db'
+
+####################
+# Helper functions
+####################
+
+
+def update_db(chatid, newentries):
+    idasstring = str(chatid)
+
+    with shelve.open(db_file) as db:
+        oldentries = None
+        try:
+            oldentries = db[idasstring]
+        except KeyError:
+            oldentries = {}
+        oldentries.update(newentries)
+
+        db[idasstring] = oldentries
+        logging.info(db[idasstring])
+
+
+def request_current_jackpot(species = None):
+    url = ""
+    if (species == Species.EUROJACKPOT):
+        url = "https://www.lotto.de/api/stats/entities.eurojackpot/last"
+    elif (species == Species.LOTTO):
+        url = "https://www.lotto.de/api/stats/entities.lotto/last"
+    else:
+        result = []
+        result.append(request_current_jackpot(Species.EUROJACKPOT))
+        result.append(request_current_jackpot(Species.LOTTO))
+
+        return result
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    response = requests.get(
+        url, headers=headers)
+    data = response.json()
+
+    return (data['jackpotNew'], data['drawDate'])
+
+
+class Species(Enum):
+    EUROJACKPOT = 1
+    LOTTO = 2
+
+####################
+# Callback functions
+####################
+
+
+def start(update, context):
+    logging.info(update.effective_chat.id)
+    newentry = {
+        'isactive': True
+    }
+    update_db(update.effective_chat.id, newentry)
+    update.message.reply_text(
+        "Ok! Let's get a notification for _Eurojackpot_ /help")
+
+
+def eurojackpot(update, context):
+    logging.info(update.effective_chat.id)
+
+    if (len(context.args) == 0):
+        jackpot, drawDate = request_current_jackpot(Species.EUROJACKPOT)
+        update.message.reply_text(
+            "Current eurojackpot: {} mil. €".format(jackpot))
+        return
+
+    # TODO verify args
+    bound = int(context.args[0])
+
+    user_says = " " + str(bound)
+    update.message.reply_text(
+        "You will be notified if the eurojackpot is above: " + user_says + " mil. €")
+
+    newentry = {
+        'eurojackpot_bottom_bound': bound,
+        'lastmessage': datetime(1, 1, 1)
+    }
+    update_db(update.effective_chat.id, newentry)
+
+
+def lotto(update, context):
+    logging.info(update.effective_chat.id)
+
+    if (len(context.args) == 0):
+        jackpot, drawDate = request_current_jackpot(Species.LOTTO)
+        update.message.reply_text(
+            "Current lotto jackpot: {} mil. €".format(jackpot))
+        return
+
+    # TODO verify args
+    bound = int(context.args[0])
+
+    user_says = " " + str(bound)
+    update.message.reply_text(
+        "You will be notified if the lotto jackpot is above: " + user_says + " mil. €")
+
+    newentry = {
+        'lottojackpot_bottom_bound': bound,
+        'lastmessage': datetime(1, 1, 1)
+    }
+    update_db(update.effective_chat.id, newentry)
+
+
+def poll_eurojackpot(context):
+    logging.info("polling")
+    if not os.path.exists(db_file):
+        logging.warning("database file %s does not exist", db_file)
+        return
+
+    database = {}
+    with shelve.open(db_file, flag='r', writeback=False) as db:
+        database = copy.deepcopy(dict(db))
+    jackpots = request_current_jackpot()
+
+    for key in database:
+        bound_euro = database[key].get('eurojackpot_bottom_bound') or math.inf
+        bound_lotto = database[key].get(
+            'lottojackpot_bottom_bound') or math.inf
+        lastmessage = database[key].get('lastmessage')
+        jackpot_euro = jackpots[0][0]
+        jackpot_lotto = jackpots[1][0]
+        logging.info(
+            {
+                "bound": bound_euro,
+                "eurojackpot": jackpot_euro,
+                "lottojackpot": jackpot_euro,
+                "lastmessage": lastmessage
+            }
+        )
+        if (bound_euro <= jackpot_euro):
+            # and
+            #         (lastmessage == None or
+            #          lastmessage < drawDate + timedelta(days=1))):
+            newentry = {
+                'lastmessage': datetime.now()
+            }
+            update_db(key, newentry)
+            context.bot.send_message(chat_id=key,
+                                     text="The EUROJACKPOT is at {} mil. €".format(jackpot_euro))
+        if (bound_lotto <= jackpot_lotto):
+            context.bot.send_message(chat_id=key,
+                                     text="The LOTTOJACKPOT is at {} mil. €".format(jackpot_lotto))
+
+
+def help(update, context):
+    update.message.reply_text("""
+    /start - register to use the Lotto News Bot
+/help - show this help
+/eurojackpot -
+/lottojackpot -
+""")
+
+####################
+# Main
+####################
+
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    with open('credentials.json', 'r') as infile:
+        logging.info("Reading credentials.json …")
+        credentials = json.load(infile)
+        logging.info("Done reading credentials.json")
+
+    logging.info("Startup …")
+    updater = Updater(token=credentials['telegram-bot'], use_context=True)
+    dispatcher = updater.dispatcher
+    logging.info("Ready!")
+
+    j = dispatcher.job_queue
+
+    j.run_repeating(poll_eurojackpot, interval=1800, first=0)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    eurojackpot_handler = CommandHandler('eurojackpot', eurojackpot)
+    dispatcher.add_handler(eurojackpot_handler)
+    lotto_handler = CommandHandler('lottojackpot', lotto)
+    dispatcher.add_handler(lotto_handler)
+    help_handler = CommandHandler('help', help)
+    dispatcher.add_handler(help_handler)
+
+    logging.info("Start polling")
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
